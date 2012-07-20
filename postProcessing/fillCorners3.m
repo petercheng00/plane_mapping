c875a25c1bfbc53a1ea64feb9a4720c6f3058fd1
Peter Cheng <petercheng00@gmail.com>
2012-07-19 17:31:25 -0700

    Added t-junction snapping

--- postProcessing/fillCorners3.m
+++ postProcessing/fillCorners3.m
@@ -10,7 +10,7 @@ function fillCorners3(th, th2)
 %th - threshold to find adjacent walls in X,Y direction
 %th2 - threshold to find adjacent walls in Z direction
 
-[filename, pathname] = uigetfile('*.model', 'Select *.model file', 'C:\cygwin\tmp\pcl-0.9.0\bin\');
+[filename, pathname] = uigetfile('*.model', 'Select *.model file');
 fid = fopen([pathname filename]);
 A = fscanf(fid, '%f', [inf]);
 fclose(fid);
@@ -19,19 +19,19 @@ tot_planes = A(1,1);
 pointer=2;
 planes.tot=tot_planes;
 
-for cplane=1:1:tot_planes
+for currPlaneX=1:1:tot_planes
 
-    planes.p(cplane).npoints= A(pointer,1); %Get number of points delimiting current plane
+    planes.p(currPlaneX).npoints= A(pointer,1); %Get number of points delimiting current plane
     pointer=pointer+1;
 
-    planes.p(cplane).eq=A(pointer:pointer+3,1); %Get equation describing current plane
+    planes.p(currPlaneX).eq=A(pointer:pointer+3,1); %Get equation describing current plane
     pointer=pointer+4;
 
     cpoint=1;
-    for x=1:1:planes.p(cplane).npoints %Get the points delimiting plane
-        planes.p(cplane).x(cpoint)=A(pointer,1);
-        planes.p(cplane).y(cpoint)=A(pointer+1,1);
-        planes.p(cplane).z(cpoint)=A(pointer+2,1);
+    for intersectX=1:1:planes.p(currPlaneX).npoints %Get the points delimiting plane
+        planes.p(currPlaneX).x(cpoint)=A(pointer,1);
+        planes.p(currPlaneX).y(cpoint)=A(pointer+1,1);
+        planes.p(currPlaneX).z(cpoint)=A(pointer+2,1);
         cpoint=cpoint+1;
         pointer=pointer+3;
     end
@@ -40,14 +40,14 @@ end
 %Divide walls into two sets 
 wallsX=[];
 wallsY=[];
-for cplane=1:1:tot_planes
+for currPlaneX=1:1:tot_planes
     
-    if (abs(planes.p(cplane).eq(1))>abs(planes.p(cplane).eq(2))) && (abs(planes.p(cplane).eq(1))>abs(planes.p(cplane).eq(3))) %A wall with stron X-normal component
-        wallsX=[wallsX; cplane];
+    if (abs(planes.p(currPlaneX).eq(1))>abs(planes.p(currPlaneX).eq(2))) && (abs(planes.p(currPlaneX).eq(1))>abs(planes.p(currPlaneX).eq(3))) %A wall with stron X-normal component
+        wallsX=[wallsX; currPlaneX];
     end
     
-    if (abs(planes.p(cplane).eq(2))>abs(planes.p(cplane).eq(1))) && (abs(planes.p(cplane).eq(2))>abs(planes.p(cplane).eq(3))) %A wall with stron Y-normal component
-        wallsY=[wallsY; cplane];
+    if (abs(planes.p(currPlaneX).eq(2))>abs(planes.p(currPlaneX).eq(1))) && (abs(planes.p(currPlaneX).eq(2))>abs(planes.p(currPlaneX).eq(3))) %A wall with stron Y-normal component
+        wallsY=[wallsY; currPlaneX];
     end
 
 end
@@ -55,90 +55,117 @@ end
 
 %Pair up wallsX with wallsY if they are close enough to each other, either
 %a corner or a t-junction
-for wall=1:1:size(wallsX,1)
-    wall;
-
+for wallXInd=1:1:size(wallsX,1)
     
-    for wallp=1:1:size(wallsY,1)
-        wallp;
+    for wallYInd=1:1:size(wallsY,1)
         
-        cplane=wallsX(wall);
-        cplaneY=wallsY(wallp);        
+        currPlaneX=wallsX(wallXInd);
+        currPlaneY=wallsY(wallYInd);        
         
-        maxZ = max(planes.p(cplane).z);
-        maxZ2 = max(planes.p(claneY).z);
-        minZ = min(planes.p(cplane).z);
-        minZ2 = min(planes.p(cplaneY).z);
+        maxY = max(planes.p(currPlaneX).y);
+        minY = min(planes.p(currPlaneX).y);
         
-        if abs(maxZ2-maxZ) > th2 || abs(minZ2-minZ) > th2
-            continue;
+        maxX = max(planes.p(currPlaneY).x);
+        minX = min(planes.p(currPlaneY).x);
+        
+        maxZ = max(planes.p(currPlaneX).z);
+        maxZ2 = max(planes.p(currPlaneY).z);
+        minZ = min(planes.p(currPlaneX).z);
+        minZ2 = min(planes.p(currPlaneY).z);
+        
+        %if abs(maxZ2-maxZ) > th2 || abs(minZ2-minZ) > th2
+        %    continue;
+        %end
+        
+        
+        [intersectX intersectY]=vertWallIntersection(planes.p(currPlaneX).eq, planes.p(currPlaneY).eq);
+        
+        pointer=find(planes.p(currPlaneX).y==minY);
+        minYPoint=[planes.p(currPlaneX).x(pointer(1)), minY];
+            
+        pointer=find(planes.p(currPlaneX).y==maxY);
+        maxYPoint=[planes.p(currPlaneX).x(pointer(1)), maxY];
+        
+        pointer=find(planes.p(currPlaneY).x==minX);
+        minXPoint=[minX, planes.p(currPlaneY).y(pointer(1))];
+            
+        pointer=find(planes.p(currPlaneY).x==maxX);
+        maxXPoint=[maxX, planes.p(currPlaneY).y(pointer(1))];        
+        
+        %valid cases to join
+        %corner: p1 is near intersection, p2 is near intersection
+        %t-junction: p1 is near intersection, intersection inside other
+        %plane
+        
+        
+        pointsPlaneX = [minYPoint;maxYPoint];
+        pointsPlaneY = [minXPoint;maxXPoint];
+        
+        distancesPlaneX = sqrt((pointsPlaneX(:,1) - intersectX).^2 + (pointsPlaneX(:,2) - intersectY).^2);
+        distancesPlaneY = sqrt((pointsPlaneY(:,1) - intersectX).^2 + (pointsPlaneY(:,2) - intersectY).^2);
+        
+        inBetweenPlaneX = minY <= intersectY && intersectY <= maxY;
+        inBetweenPlaneY = minX <= intersectX && intersectX <= maxX;
+        
+        minDistPlaneX = th+1;
+        yVal = -1;
+        minDistPlaneY = th+1;
+        xVal = -1;
+        if distancesPlaneX(1) <= th && ((min(distancesPlaneY) <= th) || inBetweenPlaneY)
+            minDistPlaneX = distancesPlaneX(1);
+            yVal = minY;
+        end
+        if distancesPlaneX(2) <= min(th, minDistPlaneX) && ((min(distancesPlaneY) <= th) || inBetweenPlaneY)
+            minDistPlaneX = distancesPlaneX(2);
+            yVal = maxY;
+        end
+        if distancesPlaneY(1) <= th && ((min(distancesPlaneX) <= th) || inBetweenPlaneX)
+            minDistPlaneY = distancesPlaneY(1);
+            xVal = minX;
+        end
+        if distancesPlaneY(2) <= min(th, minDistPlaneY) && ((min(distancesPlaneX) <= th) || inBetweenPlaneX)
+            minDistPlaneY = distancesPlaneY(2);
+            xVal = maxX;
         end
         
         
-        [x y]=vertWallIntersection(planes.p(cplane).eq, planes.p(cplaneY).eq);
-        
-        pointer=find(planes.p(cplane).y==minY);
-        minYPoint=[planes.p(cplane).x(pointer(1)), minY];
-            
-        pointer=find(planes.p(cplane).y==maxY);
-        maxYPoint=[planes.p(cplane).x(pointer(1)), maxY];
-        
-        pointer=find(planes.p(cplaneY).y==minY2);
-        minY2Point=[planes.p(cplaneY).x(pointer(1)), minY2];
-            
-        pointer=find(planes.p(cplaneY).y==maxY2);
-        maxY2Point=[planes.p(cplaneY).x(pointer(1)), maxY2];        
-        
-        points1 = [minYPoint;maxYPoint];
-        points2 = [minY2Point;maxY2Point];
-        distances1 = sqrt((points1(:,1) - x)^2 + (points1(:,2) - y)^2);
-        distances2 = sqrt((points2(:,1) - x)^2 + (points2(:,2) - y)^2);
-        
-        minDist1, minInd1 = min(distances1);
-        minDist2, minInd2 = min(distances2);
-        if minDist1 <= th
+        if minDistPlaneX <= th
             %extend plane 1 to intersection line
-            if minInd1 == 1
-                val = minY;
-            else
-                val = maxY;
-            end
+
             
-            
-            pointer = find(planes.p(cplane).y==val); % points with same maxY values in cplane        
+            pointer = find(planes.p(currPlaneX).y==yVal);  
             for k=1:1:size(pointer,2)
-                p=[planes.p(cplane).x(pointer(k)) planes.p(cplane).y(pointer(k)) planes.p(cplane).z(pointer(k))];
+                p=[planes.p(currPlaneX).x(pointer(k)) planes.p(currPlaneX).y(pointer(k)) planes.p(currPlaneX).z(pointer(k))];
+                p1 = [intersectX, intersectY, -100];
+                p2 = [intersectX, intersectY, 100];
                 projPt = point2Line3D(p,p1,p2);
-                %replace points with porjected ones
-                planes.p(cplane).x(pointer(k))=projPt(1);
-                planes.p(cplane).y(pointer(k))=projPt(2);
-                if projPt(3)~= planes.p(cplane).z(pointer(k))
-                    disp('Adjusting z');
-                end
-                    %planes.p(cplane).z(pointer(k))=projPt(3);
+                %replace points with projected ones
+                planes.p(currPlaneX).x(pointer(k))=projPt(1);
+                planes.p(currPlaneX).y(pointer(k))=projPt(2);
+                %if projPt(3)~= planes.p(currPlaneX).z(pointer(k))
+                %    disp('Adjusting z');
+                %end
+                %planes.p(currPlaneX).z(pointer(k))=projPt(3);
             end
             
         end
-        if minDist2 <= th
+        if minDistPlaneY <= th
             %extend plane 1 to intersection line
-            if minInd2 == 1
-                val = minY2;
-            else
-                val = maxY2;
-            end
             
             
-            pointer = find(planes.p(cplaneY).y==val); % points with same maxY values in cplane        
+            pointer = find(planes.p(currPlaneY).x==xVal);
             for k=1:1:size(pointer,2)
-                p=[planes.p(cplaneY).x(pointer(k)) planes.p(cplaneY).y(pointer(k)) planes.p(cplaneY).z(pointer(k))];
+                p=[planes.p(currPlaneY).x(pointer(k)) planes.p(currPlaneY).y(pointer(k)) planes.p(currPlaneY).z(pointer(k))];
+                p1 = [intersectX, intersectY, -100];
+                p2 = [intersectX, intersectY, 100];
                 projPt = point2Line3D(p,p1,p2);
-                %replace points with porjected ones
-                planes.p(cplaneY).x(pointer(k))=projPt(1);
-                planes.p(cplaneY).y(pointer(k))=projPt(2);
-                if projPt(3)~= planes.p(cplaneY).z(pointer(k))
-                    disp('Adjusting z');
-                end
-                    %planes.p(cplane).z(pointer(k))=projPt(3);
+                %replace points with projected ones
+                planes.p(currPlaneY).x(pointer(k))=projPt(1);
+                planes.p(currPlaneY).y(pointer(k))=projPt(2);
+                %if projPt(3)~= planes.p(currPlaneXY).z(pointer(k))
+                %    disp('Adjusting z');
+                %end
+                %planes.p(currPlaneX).z(pointer(k))=projPt(3);
             end
             
         end
@@ -157,13 +184,13 @@ fprintf(fid, '%i\n',planes.tot); %Total number of planes
 pointer=2;
 tot_planes=planes.tot;
 
-for cplane=1:1:tot_planes
+for currPlaneX=1:1:tot_planes
 
-    fprintf(fid, '%i\n',planes.p(cplane).npoints); %Save number of points delimiting current plane
-    fprintf(fid, '%f %f %f %f\n', planes.p(cplane).eq(1,1), planes.p(cplane).eq(2,1),planes.p(cplane).eq(3,1),planes.p(cplane).eq(4,1)); %Save equation describing current plane
+    fprintf(fid, '%i\n',planes.p(currPlaneX).npoints); %Save number of points delimiting current plane
+    fprintf(fid, '%f %f %f %f\n', planes.p(currPlaneX).eq(1,1), planes.p(currPlaneX).eq(2,1),planes.p(currPlaneX).eq(3,1),planes.p(currPlaneX).eq(4,1)); %Save equation describing current plane
 
-    for cpoint=1:1:planes.p(cplane).npoints %Save the points delimiting plane
-        fprintf(fid, '%f %f %f\n',planes.p(cplane).x(cpoint), planes.p(cplane).y(cpoint),planes.p(cplane).z(cpoint));
+    for cpoint=1:1:planes.p(currPlaneX).npoints %Save the points delimiting plane
+        fprintf(fid, '%f %f %f\n',planes.p(currPlaneX).x(cpoint), planes.p(currPlaneX).y(cpoint),planes.p(currPlaneX).z(cpoint));
     end
     
 end
@@ -172,17 +199,18 @@ fclose(fid2);
 
 figure;
 
-for cplane=1:1:tot_planes
+for currPlaneX=1:1:tot_planes
 
-
-        vert=[planes.p(cplane).x', planes.p(cplane).y',planes.p(cplane).z']; 
-        fac=[1:1:planes.p(cplane).npoints];
-        patch('vertices', vert,'faces',fac,'facecolor',[(cplane/(tot_planes*2)) (cplane/tot_planes) (cplane/tot_planes)]); hold on;
+    if planes.p(currPlaneX).eq(3) == 0
+        vert=[planes.p(currPlaneX).x', planes.p(currPlaneX).y',planes.p(currPlaneX).z']; 
+        fac=[1:1:planes.p(currPlaneX).npoints];
+        patch('vertices', vert,'faces',fac,'facecolor',[(currPlaneX/(tot_planes*2)) (currPlaneX/tot_planes) (currPlaneX/tot_planes)]); hold on;
         view(3); 
         daspect([1 1 1]);
         axis('tight');
         disp('Press any key to continue');
         pause    
+    end
 
 end
 
